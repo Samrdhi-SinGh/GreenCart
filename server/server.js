af1860e88cec375831d037ed69a115e90921b097
Samrdhi-SinGh <singhsamridhi878@gmail.com>
2026-06-09 20:15:20 +0530

    Change in server.js

--- server/server.js
+++ server/server.js
@@ -20,34 +20,14 @@ await connectCloudinary()
 
 
  // Allow multiple origins
- //const allowedOrigins= ['http://localhost:5173']
+ const allowedOrigins= ['http://localhost:5173']
 
- app.set('trust proxy', 1);
-
-const allowedOrigins = [
-  'http://localhost:5173',
-  process.env.FRONTEND_URL,
-  'https://greencart-frontend-0thz.onrender.com',
-].filter(Boolean);
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    callback(new Error(`CORS policy does not allow access from origin ${origin}`));
-  },
-  credentials: true,
-};
 
  // Middleware configuration
-//  app.use(express.json()); 
-//  app.use(cookieParser());
-//  app.use(cors({origin: allowedOrigins, credentials: true}));
+  app.use(express.json()); 
+  app.use(cookieParser());
+ app.use(cors({origin: allowedOrigins, credentials: true}));
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors(corsOptions));
 
  app.get('/', (req, res) => res.send(" API is Working"));
  app.use('/api/user', userRouter)
